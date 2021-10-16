2cc521e3c767bb1e221fd993b796176dbd3a81f7
Erik Christianson <internet.erik@gmail.com>
2021-10-15 20:13:24 -0500

    tweaking some configs, updating packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 /*
  * Create variables for our project paths so we can change in one place
  */
-var paths = {
+const paths = {
   'sass'     : './scss/**/*.scss',
   'css'      : './css/',
 };
@@ -12,15 +12,18 @@ var paths = {
 /**
  * Builds the scss files
  */
-gulp.task('sass', function(){
-  return gulp.src(paths.sass)
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(gulp.dest(paths.css));
-});
+const sassBuild = cb => {
+  return src(paths.sass)
+  .pipe(sass({errLogToConsole: true}))
+  .pipe(dest(paths.css));
+}
 
-gulp.task('default', ['sass'], function() {});
+const watcher = cb => {
+  watch(paths.sass, sassBuild);
+}
 
-gulp.task('watch', function() {
-    gulp.start('sass');
-    gulp.watch(paths.sass, ['sass']);
-});
+exports.sass = sassBuild;
+
+exports.watch = series(sassBuild, watcher)
+
+exports.default = series(sassBuild);
